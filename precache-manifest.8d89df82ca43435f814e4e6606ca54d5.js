11fd203703bc6c34dfabc79aac24378cd8e88aba
Bhavya Cheruku <bhavya8182@gmail.com>
2019-08-21 17:19:39 -0400

    Update precache-manifest.8d89df82ca43435f814e4e6606ca54d5.js

--- precache-manifest.8d89df82ca43435f814e4e6606ca54d5.js
+++ precache-manifest.8d89df82ca43435f814e4e6606ca54d5.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "e27e9df9c92ff6abdffc",
-    "url": "/website/static/js/runtime~main.e27e9df9.js"
+    "url": "/static/js/runtime~main.e27e9df9.js"
   },
   {
     "revision": "5b77d81931e8c4e079ae",
-    "url": "/website/static/js/main.5b77d819.chunk.js"
+    "url": "/static/js/main.5b77d819.chunk.js"
   },
   {
     "revision": "5ec21d738fba382f2658",
-    "url": "/website/static/js/2.5ec21d73.chunk.js"
+    "url": "/static/js/2.5ec21d73.chunk.js"
   },
   {
     "revision": "5b77d81931e8c4e079ae",
-    "url": "/website/static/css/main.6903e6cb.chunk.css"
+    "url": "/static/css/main.6903e6cb.chunk.css"
   },
   {
     "revision": "bfd982a44f7401296a3d63c1c179992e",
-    "url": "/website/index.html"
+    "url": "/index.html"
   }
 ];
