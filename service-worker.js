70952f2350d0ffa36cd26cb23cd7a3d4a3883bfd
Cheruku, Bhavya (US - Princeton) <bhcheruku@deloitte.com>
2020-01-22 22:19:28 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d89df82ca43435f814e4e6606ca54d5.js"
+  "/bhavyacheruku/website/precache-manifest.e77e8d4dd31160f3f40bbd872d3c6cda.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/bhavyacheruku/website/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
